8bfe369eadf1764bf73a46b6cc5f2c66672cd49b
TableStar <afraroad@gmail.com>
2025-11-06 14:17:00 +0700

    feat: init zustand store and types

--- store/store.ts
+++ store/store.ts
@@ -1,10 +1,26 @@
 // Global state store - add your state management here
-// Example: import { create } from 'zustand';
+import { WeatherState, WeatherStore } from '@/types/weatherType';
+import { create } from 'zustand';
 
 // export interface AppState {
 //   // Add your state properties here
 // }
 
-// export const useStore = create<AppState>((set) => ({
-//   // Add your state and actions here
-// }));
+const initialState:WeatherState = {
+  place: null,
+  apiResponse:null,
+  currWeather: null,
+  dailyforecasts: [],
+  isLoading: true,
+  error: null,
+};
+
+export const useWeather = create<WeatherStore>((set) => ({
+    ...initialState,
+    fetchWeatherByCoords: async (lat, lon) => {
+        console.log(lat,lon);
+    },
+    fetchWeatherByCity: async (city) => {
+        console.log(city);
+    },
+}));
